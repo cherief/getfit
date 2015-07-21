25761dc1d4c718d004a1478fc3e4f6d0bcb8d73c
Cherie <c.k.fishlock@gmail.com>
2015-07-21 19:33:35 +1000

    fixed both versions

--- save_json_v2.php
+++ save_json_v2.php
@@ -1,23 +1,45 @@
-<?php
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"> 
 
-// print_r($_POST);
+<html>
+    <head>
 
+        <title>Fit v1.0</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="style.css" rel="stylesheet" type="text/css">
 
-// $url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-// $parsed_url = parse_url($url,PHP_URL_QUERY);
+    </head>
 
-// parse_str($parsed_url,$arr);
+    <body>
 
-$jsondata = file_get_contents('general.json');
-$tempArray = json_decode($jsondata);
+       <?php
 
-// $newarr = array("date" => $arr["date"], "treadmill" => $arr["treadmill"], "rower"=>$arr["rower"]);
+        $url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+        $parsed_url = parse_url($url,PHP_URL_QUERY);
 
-$newarr = $_POST;
+        parse_str($parsed_url,$arr);
 
-array_push($tempArray -> fit,$newarr);
+        $jsondata = file_get_contents('getfit.json');
+        $tempArray = json_decode($jsondata);
 
-$newjson = json_encode($tempArray);
-file_put_contents('general.json', $newjson);
+        $newarr = array("date" => $arr["date"], "treadmill" => $arr["treadmill"], "rower"=>$arr["rower"]);
 
-?>
+        echo "<div id='summary'>Summary</div>";
+
+        echo "<div id='summ_equip'>";
+
+            echo "<div id='summ_tread'>Treadmill: ", $arr["treadmill"], " km</div>";
+
+            echo "<div id='summ_row'>Rower: ", $arr["rower"], " m</div>";
+
+        echo "</div>";    
+
+        array_push($tempArray -> fit,$newarr);
+
+        $newjson = json_encode($tempArray);
+        file_put_contents('getfit.json', $newjson);
+
+        ?> 
+
+    </body>
+
+</html>
